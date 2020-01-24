c0165ef3c24cad5721f4f3a1ccddb1a80a37a08e
markubjo <markusbjon@gmail.com>
2020-01-24 16:44:23 +0100

    removed bad comments

--- src/calibration.py
+++ src/calibration.py
@@ -8,17 +8,12 @@ from utils import *
 np.set_printoptions(suppress=True)
 if __name__ == '__main__':
     # 13.9
-    folder = 'captures02-12-19'
+    folder = 'datasets/captures02-12-19'
     #folder = 'datasets/dataset_zivid'
     sq = 13.8
     nx = 20
     ny = 13
     calibrator = ZividHEcalibrator(sqrSize=sq, nx=nx, ny=ny)
-
-    # calibrator.set_intrinsics('intrinsics/camera_matrix_dataset_zivid.txt',
-    #                           'intrinsics/distortion_params_dataset_zivid.txt')
-    # calibrator.set_intrinsics('intrinsics/camera_matrix_from_cam.txt',
-    #                           'intrinsics/distortion_params_from_cam.txt')
 
     calibrator.load_zdfs(folder)
     calibrator.load_robot_poses(folder)
@@ -27,7 +22,7 @@ if __name__ == '__main__':
     calibrator.viz_cam_pose(focus='objectCentric')
     calibrator.viz_rob_pose()
     calibrationMatrices = []
-    #for i in range(4, len(calibrator.robot_poses)+1):
+    
     Ai, Bi = calibrator.calculate_relative_poses(
         pose_pairs=-1, use_board_pts=True)
     calibrationMatrices.append(calibrator.HE_calibration(Ai, Bi))
@@ -36,9 +31,9 @@ if __name__ == '__main__':
     calibrator.viz_HE_transf(calibrationMatrices, showTranslation=True)
     print(calibrator.method)
     calibrator.calib_error()
-    #calibrator.method = '2D'
     calibrator.err_iter(plot=True)
-    # show boards with coordinate systems
+
+    # show boards with coordinate systems:
     # for imgNb in range(0, len(calibrator.point_clouds)):
     #     img = calibrator.draw_coord_sys(imgNb)
     #     cv2.imshow(f'board {imgNb}', cv2.resize(img, (0, 0), fx=0.5, fy=0.5))
